d03c15929ba04764d59ba303a490915ed35d22e1
chenhao <email@163.com>
2018-07-27 14:20:56 +0800

    ss
    ss

---
+++ school/Main/Root/SFNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  SFNavigationController.h
+//  school
+//
+//  Created by Oliver Mac on 2018/7/27.
+//  Copyright © 2018年 rl. All rights reserved.
+//
+
+#import "SFBaseNavigationController.h"
+
+@interface SFNavigationController : SFBaseNavigationController
+
+@end
