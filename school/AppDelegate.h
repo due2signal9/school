a5d6052303021f624facc0f8b0b18d26c9f93141
MnZr <mnzremail@163.com>
2018-07-27 11:57:21 +0800

    Initial Commit

---
+++ school/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  school
+//
+//  Created by Oliver Mac on 2018/7/27.
+//  Copyright © 2018年 rl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
