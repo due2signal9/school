1e107cfb40945e58ebf082f6ca803560736f7755
chenhao <email@163.com>
2018-07-30 18:38:28 +0800

    ss
    ss

---
+++ school/Main/App/Controller/SFMineHeadView.h
@@ -0,0 +1,13 @@
+//
+//  SFMineHeadView.h
+//  school
+//
+//  Created by Oliver Mac on 2018/7/30.
+//  Copyright © 2018年 rl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SFMineHeadView : UIView
+
+@end
---
+++ school/Main/App/Controller/SFMineHeadView.m
@@ -0,0 +1,21 @@
+//
+//  SFMineHeadView.m
+//  school
+//
+//  Created by Oliver Mac on 2018/7/30.
+//  Copyright © 2018年 rl. All rights reserved.
+//
+
+#import "SFMineHeadView.h"
+
+@implementation SFMineHeadView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
